76f2ef3b6757d9b8f06da7183f1e49efcb074bd3
Armando <roblesrioarmand@gmail.com>
2017-12-01 12:24:09 -0600

    functional code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,56 @@
-//$("#jewel_1").html("<img src='assets/images/if_Amber_127310.png' />");
-//$("#jewel_2").html("<img src='assets/images/if_Amethyst_127306.png' />");
-//$("#jewel_3").html("<img src='assets/images/if_Aquamarine_127303.png' />");
-//$("#jewel_4").html("<img src='assets/images/if_Emerald_127302.png' />");
+//$(function(){
 
-var number=0, guess=0, loss=0, win=0, scoreTotal=0, score=0;
-var jew1, jew2, jew3, jew4;
-var jewels
+  var number=0, guess=0, loss=0, win=0, scoreTotal=0, score=0;
+ initValues();
+  //Random number to match
+  function initValues() {
+    number = Math.floor(Math.random() * 101)+19;
+    $("#number").html(number);
+    scoreTotal=0;  
+    //Assign randon numbers to crystals
+  /*  $("img").each(function() {
+     $(this).attr("data-value", Math.floor(Math.random() * 12));
+    });*/
 
-//var scoreTotal = 0;
-console.log("scoreTotal at begginning: " + scoreTotal);
-//Random number
-number = Math.floor(Math.random() * 101)+19;
-$("#number").html(number);
-var jewels = -0;
-//Assign randon numbers to crystals
-$("img").each(function() {
-   $(this).attr("data-value", Math.floor(Math.random() * 12));
-});
+    $("#jewel_1").attr("data-value", Math.floor(Math.random() * 12));
+    $("#jewel_2").attr("data-value", Math.floor(Math.random() * 12));
+    $("#jewel_3").attr("data-value", Math.floor(Math.random() * 12));
+    $("#jewel_4").attr("data-value", Math.floor(Math.random() * 12));
+    console.log("scoreTotal inside initValue: " + scoreTotal);
+  }
 
-//Function to store the accumulated score from choosing the crystals
-function addScore (num) {
-	scoreTotal = scoreTotal + num;
-	$("#scoret").html(scoreTotal); 
-}
+  //Function to store the accumulated score from choosing the crystals
+  function addScore (num) {
+    scoreTotal = scoreTotal + num;
+    $("#scoret").html(String(scoreTotal)); 
+    if (number === scoreTotal) {
+    	win++;
+    	console.log("Win value: " + win);
+    	$("#wins").html("Wins: " + win);
+    	initValues();
+    } else if ( number < scoreTotal ){
+    	loss++;
+    	$("#loss").html("Losses: " + loss);
+    	$("#scoret").html(scoreTotal + " You Lost!");
+    	initValues();
+    }
+  };
 
-$("#jewel_1").click(function() {
-	
-	jew1 = $("#jewel_1").attr("data-value");
-	console.log("tipo: " + typeof(jew1));
-	jew1 = Number(jew1);
-	console.log("Val de jew1: " + jew1);
-    console.log("nuevo tipo: " + typeof(jew1));
-  addScore(Number(jew1));
-});
+  //Get the value of crystals and call function to add its value
+  $('#jewel_1').click(function () {
+    addScore(Number($('#jewel_1').attr("data-value")));
+  });
 
-$("#jewel_2").click(function() {
-	jew2 = $("img").attr("data-value");
-	console.log("tipo: " + typeof(jew2));
-	jew2 = Number(jew2);
-	console.log("Val de jew2: " + jew2);
-    console.log("nuevo tipo: " + typeof(jew2));
-  addScore(Number(jew2));
-});
+  $('#jewel_2').click(function () {
+    addScore(Number($('#jewel_2').attr("data-value")));
+  });
 
-$("#jewel_3").click(function() {
-  addScore(Number($("img").attr("data-value")));
-});
+  $("#jewel_3").click(function() {
+    addScore(Number($("#jewel_3").attr("data-value")));
+  });
 
-$("#jewel_4").click(function() {
-  addScore(Number($("img").attr("data-value")));
-});
+  $("#jewel_4").click(function() {
+    addScore(Number($("#jewel_4").attr("data-value")));
+  });
+ 
+//});//End ready -for JQuery-
